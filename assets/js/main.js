2f183fd0b16c27886222aff20724876b52810fff
M.Anwary <36699318+Halloween1992@users.noreply.github.com>
2021-06-22 13:21:38 +0200

    Added new files

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZWFnbGUtaHVudGVyIiwiYSI6ImNrcTd2cjFhZzA1bHcybm9lM3cwZGtiOTYifQ.FWiJNi85eMRCj54l6HZuVg";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-71.060682, 42.35725],
+  zoom: 18,
+});
